771258e89f707cb8ed5bd81cb9670294128da36e
anand maurya <anandkr.maurya10@gmail.com>
2024-08-04 16:30:31 +0530

    added

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -4,9 +4,10 @@ import flowerImg from "./image/vcall2.png";
 import lighthouseImg from "./image/react1.png";
 import dandelion from "./image/express-js.png";
 import vp from "./image/vp.png";
+import api2 from "./image/api2.png";
 
 function Slider() {
-  const images = [flowerImg, vp, lighthouseImg, flowerImg, dandelion, vp];
+  const images = [flowerImg, vp, lighthouseImg, api2, flowerImg, dandelion, vp];
 
   return (
     <div className="App">
