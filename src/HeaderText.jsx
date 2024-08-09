0e3b9c1e7c566de82d0839d1f7aeeef1a0df7f7a
anand maurya <anandkr.maurya10@gmail.com>
2024-08-09 14:44:45 +0530

    added

--- src/HeaderText.jsx
+++ src/HeaderText.jsx
@@ -17,7 +17,7 @@ function HeaderText({ theme }) {
           style={{ fontFamily: "'UnifrakturCook', cursive" }}
         >
           𝔄 𝔰𝔦𝔪𝔭𝔩𝔢 <span className="s">𝔳𝔦𝔡𝔢𝔬 𝔠𝔞𝔩𝔩𝔦𝔫𝔤 & 𝔳𝔦𝔡𝔢𝔬 𝔭𝔩𝔞𝔶𝔢𝔯</span>{" "}
-          𝔴𝔢𝔟𝔰𝔦𝔱𝔢 𝔴𝔥𝔢𝔯𝔢 𝔶𝔬𝔲 𝔠𝔞𝔫 𝔠𝔬𝔫𝔫𝔢𝔠𝔱 𝔱𝔬 𝔶𝔬𝔲𝔯
+          𝔴𝔢𝔟𝔰𝔦𝔱𝔢 𝔴𝔥𝔢𝔯𝔢 𝔶𝔬𝔲 𝔠𝔞𝔫 𝔠𝔬𝔫𝔫𝔢𝔠𝔱 𝔴𝔦𝔱𝔥 𝔶𝔬𝔲𝔯
           <span className="v"> 𝔣𝔯𝔦𝔢𝔫𝔡𝔰 </span>𝔞𝔫𝔡
           <span className="v"> 𝔣𝔞𝔪𝔦𝔩𝔶</span> 𝔞𝔫𝔡 𝔢𝔵𝔭𝔢𝔯𝔦𝔢𝔫𝔠𝔢 𝔠𝔯𝔶𝔰𝔱𝔞𝔩-𝔠𝔩𝔢𝔞𝔯 𝔳𝔦𝔡𝔢𝔬
           𝔠𝔞𝔩𝔩𝔰 𝔱𝔥𝔞𝔱 <span className="v">𝔟𝔯𝔦𝔫𝔤 𝔶𝔬𝔲 𝔠𝔩𝔬𝔰𝔢𝔯.</span>
