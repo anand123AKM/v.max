42b6ceaa4ec38cd9148c46e231bc5e9daf085fed
anand maurya <anandkr.maurya10@gmail.com>
2024-08-09 14:05:25 +0530

    added

--- src/About.jsx
+++ src/About.jsx
@@ -9,9 +9,17 @@ function About({ theme, setTabIndex }) {
       <Center>
         <Heading className={`ABHEAD ${theme}`}>ᴀʙᴏᴜᴛ</Heading>
       </Center>
-      <h1 className={`${theme} intro`}>𝐈𝐧𝐭𝐫𝐨𝐝𝐮𝐜𝐭𝐢𝐨𝐧</h1>
+      <h1
+        className={`${theme} intro`}
+        style={{ fontFamily: "'UnifrakturCook', cursive" }}
+      >
+        𝐈𝐧𝐭𝐫𝐨𝐝𝐮𝐜𝐭𝐢𝐨𝐧
+      </h1>
       <Center>
-        <Text className={`${theme} text`}>
+        <Text
+          className={`${theme} text`}
+          style={{ fontFamily: "'UnifrakturCook', cursive" }}
+        >
           ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ᴜsᴇʀ-ғʀɪᴇɴᴅʟʏ ᴠɪᴅᴇᴏ ᴘʟᴀᴛғᴏʀᴍ, ᴅᴇsɪɢɴᴇᴅ ᴛᴏ ʙʀɪɴɢ ʏᴏᴜ ᴀɴᴅ
           ʏᴏᴜʀ ʟᴏᴠᴇᴅ ᴏɴᴇs ᴄʟᴏsᴇʀ ᴛʜʀᴏᴜɢʜ ᴛʜᴇ ᴘᴏᴡᴇʀ ᴏғ ᴄʟᴇᴀʀ ᴀɴᴅ sᴇᴄᴜʀᴇ ᴠɪᴅᴇᴏ
           ᴄᴀʟʟs. ᴇɴᴊᴏʏ sᴛʀᴇᴀᴍɪɴɢ ᴠɪᴅᴇᴏs ғʀᴏᴍ ᴀ ᴠᴀʀɪᴇᴛʏ ᴏғ ɢᴇɴʀᴇs ᴡɪᴛʜ ᴇᴀsᴇ, ᴀʟʟ
@@ -20,17 +28,14 @@ function About({ theme, setTabIndex }) {
       </Center>
       <h1 className={`${theme} intro`}>𝐏𝐮𝐫𝐩𝐨𝐬𝐞</h1>
       <Center>
-        <Text className={`${theme} text`}>
-          ᴏᴜʀ ᴍɪssɪᴏɴ ɪs sɪᴍᴘʟᴇ: ᴛᴏ ᴄʀᴇᴀᴛᴇ ᴀ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ᴛʜᴀᴛ ɢᴏᴇs ʙᴇʏᴏɴᴅ
-          ᴅɪsᴛᴀɴᴄᴇs. ᴡɪᴛʜ ᴏᴜʀ ᴘʟᴀᴛғᴏʀᴍ, ʏᴏᴜ ᴄᴀɴ ᴡᴀᴛᴄʜ ᴠɪᴅᴇᴏs, ɢᴇᴛ ᴇɴᴛᴇʀᴛᴀɪɴᴇᴅ,
-          ᴀɴᴅ ᴇᴀʀɴ ᴘᴏɪɴᴛs ᴛʜᴀᴛ ᴄᴀɴ ʀᴇᴅᴇᴇᴍ ᴇxᴄʟᴜsɪᴠᴇ ʀᴇᴡᴀʀᴅs. ɴᴏᴛ ᴏɴʟʏ ᴛʜᴀᴛ, ʏᴏᴜ
-          ᴄᴀɴ ᴇᴀsɪʟʏ ᴄᴏɴɴᴇᴄᴛ ᴡɪᴛʜ ғʀɪᴇɴᴅs ᴀɴᴅ ғᴀᴍɪʟʏ ᴛʜʀᴏᴜɢʜ ᴏᴜʀ sᴇᴄᴜʀᴇ ᴠɪᴅᴇᴏ
-          ᴄᴀʟʟs ᴀɴᴅ sʜᴀʀᴇ ʏᴏᴜʀ sᴄʀᴇᴇɴ ᴛᴏ ᴇɴᴊᴏʏ sᴘᴇᴄɪᴀʟ ᴍᴏᴍᴇɴᴛs ᴛᴏɢᴇᴛʜᴇʀ.
-        </Text>
+        <Text className={`${theme} text`}></Text>
       </Center>
       <h1 className={`${theme} intro`}>𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬</h1>
       <Center>
-        <Text className={`${theme} text`}>
+        <Text
+          className={`${theme} text`}
+          style={{ fontFamily: "'UnifrakturCook', cursive" }}
+        >
           ᴏᴜʀ ᴘʟᴀᴛғᴏʀᴍ ɪs ʟᴏᴀᴅᴇᴅ ᴡɪᴛʜ ғᴇᴀᴛᴜʀᴇs ᴛᴏ ᴇɴʜᴀɴᴄᴇ ʏᴏᴜʀ ᴇxᴘᴇʀɪᴇɴᴄᴇ. ғʀᴏᴍ
           ᴛʜᴇ ʜᴏᴍᴇ ᴘᴀɢᴇ, ʏᴏᴜ ᴄᴀɴ ᴇxᴘʟᴏʀᴇ ᴠᴀʀɪᴏᴜs sᴇᴄᴛɪᴏɴs, sᴛʀᴇᴀᴍ ᴠɪᴅᴇᴏs ᴏɴʟɪɴᴇ,
           ᴀɴᴅ ʟᴇᴀᴠᴇ ᴄᴏᴍᴍᴇɴᴛs ᴛᴏ sʜᴀʀᴇ ʏᴏᴜʀ ᴛʜᴏᴜɢʜᴛs ᴡɪᴛʜ ᴏᴛʜᴇʀs. ᴠɪᴅᴇᴏ ᴄᴀʟʟs ᴀʀᴇ
@@ -40,11 +45,14 @@ function About({ theme, setTabIndex }) {
       </Center>
       <h1 className={`${theme} intro`}>𝐂𝐨𝐧𝐭𝐚𝐜𝐭</h1>
       <Center>
-        <Text className={`${theme} text`}>
+        <Text
+          className={`${theme} text`}
+          style={{ fontFamily: "'UnifrakturCook', cursive" }}
+        >
           ʜᴀᴠᴇ ᴀ ǫᴜᴇsᴛɪᴏɴ, ɪᴅᴇᴀ, ᴏʀ sᴜɢɢᴇsᴛɪᴏɴ? ᴡᴇ'ᴅ ʟᴏᴠᴇ ᴛᴏ ʜᴇᴀʀ ғʀᴏᴍ ʏᴏᴜ!
           ᴄᴏɴᴛᴀᴄᴛ ᴜs ᴅɪʀᴇᴄᴛʟʏ ᴀɴᴅ ʟᴇᴛ's ᴄʜᴀᴛ.
           <Button onClick={() => setTabIndex(1)} w={20} h={5} ml={5}>
-            ᴍᴇssᴀɢᴇ ᴜs
+            ᴍᴇssᴀɢᴇ
           </Button>
         </Text>
       </Center>
